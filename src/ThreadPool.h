e4f7c0d1b4876ebca1715278e7eff9f5f1ba5455
Morten Kristensen <msk@nullpointer.dk>
2015-09-06 18:27:58 +0200

    Guard for zero or negative thread count.

--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -2,12 +2,11 @@
 #define THREAD_POOL_H
 
 #include <queue>
-#include <thread>
 #include <vector>
 #include <future>
 #include <functional>
 
-template <typename RetType>
+template <typename RetType> ///< Return type of tasks.
 class ThreadPool {
 public:
   using Task = std::function<RetType()>;
@@ -17,7 +16,9 @@ private:
   using FutVec = std::vector<std::future<RetType>>;
 
 public:
-  ThreadPool(int threads = std::thread::hardware_concurrency());
+  /// -1 threads means to use as many threads as there are hardware threads on
+  /// the system.
+  ThreadPool(int threads = -1);
 
   void queueTask(Task task);
   void process();
--- src/ThreadPool.hpp
+++ src/ThreadPool.hpp
@@ -1,7 +1,17 @@
 #ifdef THREAD_POOL_H
 
+#include <thread>
+
 template <typename RetType>
-ThreadPool<RetType>::ThreadPool(int threads) : threads(threads) { }
+ThreadPool<RetType>::ThreadPool(int threads) {
+  if (threads == -1) {
+    threads = std::thread::hardware_concurrency();
+  }
+  if (threads <= 0) {
+    threads = 1;
+  }
+  this->threads = threads;
+}
 
 template <typename RetType>
 void ThreadPool<RetType>::queueTask(Task task) {
